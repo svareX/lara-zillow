35db0d576bac854dac42d42ccb7eafc48f2ca5c8
svareX <stepanecek2003@gmail.com>
2023-07-20 09:20:36 +0200

    PageNotFound

---
+++ resources/js/Pages/Auth/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col gap-4 justify-center items-center py-96">
+    <h1 class="text-4xl">
+      Unfortunatelly the page you were looking for couldn't be found.
+    </h1>
+    <Link :href="route('listing.index')" class="btn-primary text-2xl">Home</Link>
+  </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
--- routes/web.php
+++ routes/web.php
@@ -61,3 +61,8 @@ Route::post('/email/verification-notification', function (Request $request) {
 
     return back()->with('message', 'Verification link sent!');
 })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
+//Fallback
+Route::fallback(function () {
+    return inertia('Auth/NotFound');
+});
